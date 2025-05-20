4d0b0e9c8f65f6636d6abc671cb295e0560ff43d
nicolasnocete <nicolasnocete@gmail.com>
2025-05-20 18:59:22 -0300

    Update game scene and assets for Phaser 3 Tilemap Template
    - Changed title in index.html for clarity
    - Updated game configuration in main.js (scene name and dimensions)
    - Removed unused assets and added new assets (bomb, dude, star, texture)
    - Introduced Game.js scene with tilemap loading and player mechanics
    - Deleted HelloWorldScene.js as it is no longer needed

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import HelloWorldScene from "./scenes/HelloWorldScene.js";
+import Game from "./scenes/Game.js";
 
 // Create a new Phaser config object
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 720,
+  height: 720,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
@@ -27,7 +27,7 @@ const config = {
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [HelloWorldScene],
+  scene: [Game],
 };
 
 // Create a new Phaser game instance
